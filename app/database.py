8d920062264ca49b9f4e9053e67a87371dd46b5e
BillyMuthiani <muthianibilly@gmail.com>
2026-02-24 16:17:44 +0300

    Use Railway DATABASE_URL

--- app/database.py
+++ app/database.py
@@ -1,9 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgres:admin@localhost:5432/goldenboot"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
